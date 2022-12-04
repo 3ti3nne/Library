75ffc60253fa865e69ddc577a78afb38ed8d2e1c
Etienne ROUBAUD <eti3nne.roubaud@gmail.com>
2022-12-04 20:48:41 +0100

    //

--- build/js/script.js
+++ build/js/script.js
@@ -40,20 +40,26 @@ submitBtn.addEventListener("click", (e) => {
     library.forEach((book) => {
       card = `
       <div class="w-40 h-56 p-3 bg-slate-100 rounded-lg shadow-md grid grid-flow-row gap-2 place-content-center content-center justify-center hover:scale-105 duration-300 ease-in-out">
-      <h5 class="font-bold mb-1">${book.title}</h5>
-      <p class="text-gray-700 text-base justify-self-center">${book.author}</p>
-      <p class="text-gray-700 text-base justify-self-center">${
-        book.pages
-      } pages</p>
-      <button id="readBtn" class="${
-        book.read ? "bg-emerald-300" : "bg-red-500"
-      } text-white w-14 px-3 font-bold rounded-full justify-self-center">${
-        book.read ? "Fini" : "En cours"
-      }</button>
-      <button id="deleteBtn" data-book-title="${
-        book.title
-      }" class="bg-black hover:bg-red-700 text-white w-10 font-bold rounded-full justify-self-center">X</button>
-      
+          
+        <h5 class="font-bold mb-1">${book.title}</h5>
+
+          <p class="text-gray-700 text-base justify-self-center">${
+            book.author
+          }</p>
+
+          <p class="text-gray-700 text-base justify-self-center">
+          ${book.pages} pages
+          </p>
+          <button id="readBtn" class="
+          ${book.read ? "bg-emerald-300" : "bg-red-500"}
+        text-white w-14 px-3 font-bold rounded-full justify-self-center">
+          ${book.read ? "Fini" : "En cours"}
+          </button>
+          <button id="deleteBtn" data-book-title="
+          ${book.title}"
+          class="bg-black hover:bg-red-700 text-white w-10 font-bold rounded-full justify-self-center">
+          X
+          </button>
       </div>`;
     });
     placeToAppendCars.innerHTML += card;
@@ -70,15 +76,13 @@ d.addEventListener("click", (e) => {
   const target = e.target.closest("#deleteBtn");
   if (target) {
     if (target.parentNode) {
-      console.log(target.parentNode);
       target.parentNode.parentNode.removeChild(target.parentNode);
     }
     const findIttarget = target.dataset.bookTitle;
 
     function removeBookByTitle(value, index, arr) {
-      // If the value at the current array index matches the specified value (2)
       if (value.title === findIttarget) {
-        // Removes the value from the original array
+        // Removes value from the original array
         arr.splice(index, 1);
         return true;
       }
